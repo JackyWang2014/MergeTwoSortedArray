20767bfdc9fc872f0fbcaba17ce8dbed682d358f
JackyWang <339847016@qq.com>
2016-05-20 00:21:02 +0800

    Merging two sorted array in two methods

--- Merge_Two_sortedArr/main.c
+++ Merge_Two_sortedArr/main.c
@@ -7,9 +7,60 @@
 //
 
 #include <stdio.h>
-
+#define M 5
+#define N 3
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+    
+    int a[M] = {10,30,50,70,90};
+    int b[N] = {4,6,8};
+    
+    int c[M+N];
+    int i = 0,j = 0,k = 0;
+    
+    for(int z = 0; z < M+N;z++ )
+    {
+        if (j >= N) {
+            c[k++] = a[i++];
+        } else if(i >= M) {
+            c[k++] = b[j++];
+        } else if ( a[i] > b[j]) {
+            c[k++] = b[j++];
+        } else {
+            c[k++] = a[i++];
+        }
+    }
+    
+    
+    for (int i = 0; i < M+N; i++) {
+        printf("%d ",c[i]);
+    }
+    putchar(10);
+    
+    return 0;
+}
+
+int test()
+{
+    int a[M] = {10,30,50,70,90};
+    int b[N] = {4,6,8};
+    
+    int c[M+N];
+    int i = 0,j = 0,k = 0;
+    while (i < M && j < N) {
+        if (a[i] < b[j]) {
+            c[k++] = a[i++];
+        } else {
+            c[k++] = b[j++];
+        }
+    }
+    
+    while (i < M) {
+        c[k++] = a[i++];
+    }
+    
+    while (j < N) {
+        c[k++] = b[j++];
+    }
+    
     return 0;
 }
