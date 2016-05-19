8945b514ca3d02144b070a9f0ecd9889c10410a4
JackyWang2014 <339847016@qq.com>
2016-05-20 00:05:36 +0800

    merge sorted array in 2 methods

---
+++ Merge_Two_sortedArr/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Merge_Two_sortedArr
+//
+//  Created by 王亓泰 on 16/5/20.
+//  Copyright © 2016年 WangQitai. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
